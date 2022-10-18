68fd9993f238cf048102df5876a42eb2d6ce69b4
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-10-18 22:57:27 +0500

    string_len10.py

--- string_len10.py
+++ string_len10.py
@@ -2,10 +2,11 @@ def main(s):
     """
     A string of length three is given. Check that it is a palindrome.
     Return True if the palindrome is False otherwise
-
+    
     Args:
         s: str
     Returns:
         bool: answer
     """
-    return
+    return s[::-1 ]==s
+print(main('asa'))
