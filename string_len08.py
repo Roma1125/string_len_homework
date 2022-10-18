027c403ec4172f698c45f53ea581ef32145d799a
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-10-18 22:21:40 +0500

    string_len08.py

--- string_len08.py
+++ string_len08.py
@@ -8,4 +8,9 @@ def main(s):
     Returns:
         str: answer
     """
-    return
+    if len(s)%2==1:
+        d=s[len(s)//2]
+    elif len(s)%2==0:
+        d=s[((len(s)//2)-1):((len(s)//2)+1)]
+    return d 
+print(main('codemm'))
